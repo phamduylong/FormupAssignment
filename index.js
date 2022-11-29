92e126c61928cdabd5d91c936777070264a6c169
longph <longphamduy2002@gmail.com>
2022-11-29 23:41:36 +0200

    function working, wrapper needed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your solution here
+import getPlayerScores from "./playerScores.js";
+const winner_list = [];
+const win_counts = [];
+const matches = await getPlayerScores();
+for (let i = 0; i < matches.length; ++i) {
+    //players and scores stored in arrays in respective orders
+    const players = Object.keys(matches[i]);
+    const res = Object.values(matches[i]);
+    //count a game only if there's a winner, ignore tie matches
+    if (res[0] !== res[1]) winner_list.push((res[0] > res[1] ? players[0] : players[1]));
+}
+
+//accumulate total number of wins for each player
+for (const num of winner_list) {
+    win_counts[num] = win_counts[num] ? win_counts[num] + 1 : 1;
+}
+
+//sorting and keeping players name only
+const sortedLeaderboard = Object.keys(win_counts).sort((a,b) => { return win_counts[b] - win_counts[a]; });
+
