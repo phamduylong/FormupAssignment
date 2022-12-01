dbf9a0bff7cfb9daa3b13c0dae8d1012811b8f86
longph <longphamduy2002@gmail.com>
2022-12-01 10:19:34 +0200

    added return value testing

--- test.js
+++ test.js
@@ -1,15 +1,30 @@
-import { ok, fail } from 'assert';
+import { ok } from 'assert';
+import { assert } from 'chai';
 import playersByScore from './index.js';
 const test_arr = await playersByScore();
 describe('Array', () => {
-    describe('Array', () => {
+    describe('Return value from function', () => {
+        it('Return value should be an array', () => {
+            assert(Array.isArray(test_arr), "Function successfully return an array");
+        });
+
+        it('Should not be undefined', () => {
+            assert.notStrictEqual(test_arr, undefined, "Array is defined");
+        });
+    });
+    describe('Array contents', () => {
         it('Array size must be less than or equal to 8', () => {
-            ok(test_arr.length <= 8, "Leaderboard contains more names than the amount of players");
+            assert.isAtMost(test_arr.length, 8, "Array size is 8 at most");
+            //below is an assertion test that does the same with node's assert module
+            //ok(test_arr.length <= 8, "Leaderboard contains more names than the amount of players");
         });
 
         it('Array should not contains duplicates', () => {
+            //creating a set from array to eliminate all duplicated members
             const uniques = new Set(test_arr);
-            ok(uniques.size === test_arr.length, "Duplicates players name on the leaderboard!");
+            assert.strictEqual(uniques.size, test_arr.length, "Set and array sizes are the same (no duplicates)");
+            //below is an assertion test that does the same with node's assert module
+            //ok(uniques.size === test_arr.length, "Duplicates players name on the leaderboard!");
         });
     });
 });
